41149d491751cab88da8ebee96b7e5163de5bad9
范勤阳 <qinyang.fan@xsuperzone.com>
2021-09-24 20:11:48 +0800

    last commit

--- main.js
+++ main.js
@@ -1,79 +1,89 @@
-// 1、创建div,存放到div这个变量里面
-// var div = document.createElement("div");
-// 2、将div作为儿子，放到id为wrapper这个div里面
-// wrapper.appendChild(div);
-// 3、利用循环，存放三个div
+// 1.初始化数据
+var data = init()["keys"]
+var hash = init()["hash"]
 
-// 定义数据结构
-var data = [
-    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
-    ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
-    ["z", "x", "c", "v", "b", "n", "m"],
-]
+// 2.生成键盘
+generayKeyboard(data, hash);
 
-// 定义 Hash 对应表
-var hash = {
-    q: "https://www.iqiyi.com",
-    w: "https://www.baidu.com" // 这里用小写的 w 试一下
+// 3.监听按键事件
+listenToUser(hash);
+
+// 4.工具函数
+function getFromLocalStorage(name) {
+    return JSON.parse(localStorage.getItem(name) || "null");
 }
 
-newHash = JSON.parse(localStorage.getItem("zzz") || 'null')
-if(newHash){
-     hash = newHash;
+function createTag(tagName, className) {
+    var tag = document.createElement(tagName);
+    for (var key in className) {
+        tag[key] = className[key];
+    }
+    return tag;
 }
 
+function init() {
+    var data = [
+        ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+        ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+        ["z", "x", "c", "v", "b", "n", "m"],
+    ];
 
-var index1 = 0;
-while (index1 < data.length) {
-    var div = document.createElement("div");
-    var className1 = "row";
-    var className2 = "row" + [index1];
-    div.classList.add(className1,className2);
-    wrapper.appendChild(div);
-    // 创建 kbd
-    var index2 = 0;
-    while (index2 < data[index1].length) {
-        var kbd = document.createElement("kbd");
-        var span = document.createElement("span");
-        var button = document.createElement("button");
-        var img = document.createElement("img")
-        kbd.className = "key";
-        span.classList.add("text")
-        span.textContent = data[index1][index2];
-        button.textContent = "编辑";
-        button.id = data[index1][index2];
-        img.src = hash[data[index1][index2]] + "/favicon.ico"
-        kbd.appendChild(span);
-        kbd.appendChild(button);
-        kbd.appendChild(img);
-        // 监听button事件
-        button.onclick = function (e) {
-            var newWebsite = prompt("请给我一个网址")
-            console.log(e);
-            hash[e["target"]["id"]] = newWebsite; // hash 变更
-            var img2 = e["target"].nextSibling; //获取 img 元素
-            img2.src = hash[e["target"]["id"]] + "/favicon.ico"
-            // 存放到localstorage
-            localStorage.setItem("zzz",JSON.stringify(hash));
+    var hash = {
+        q: "https://www.iqiyi.com",
+        w: "https://www.baidu.com", // 这里用小写的 w 试一下
+    };
+
+    newHash = getFromLocalStorage("zzz");
+    if (newHash) {
+        hash = newHash;
+    }
+
+    return {
+        "keys": data,
+        "hash": hash
+    }
+}
+
+function generayKeyboard(data, hash) {
+    for (var index1 = 0; index1 < data.length; index1++) {
+        var div = createTag("div", { className: "row" })
+        for (var index2 = 0; index2 < data[index1].length; index2++) {
+
+            var span = createTag("span", { className: "text", textContent: data[index1][index2] })
+
+            var img = document.createElement("img");
+            img.src = hash[data[index1][index2]] + "/favicon.ico";
+
+            var button = createTag("button", { textContent: "编辑", id: data[index1][index2] });
+            button.onclick = function (e) {
+                var newWebsite = prompt("请给我一个加了 http 协议的网址");
+                hash[e["target"]["id"]] = newWebsite; // hash 变更
+                var img2 = e["target"].nextSibling; //获取 img 元素
+                img2.src = hash[e["target"]["id"]] + "/favicon.ico";
+                // 存放到localstorage
+                localStorage.setItem("zzz", JSON.stringify(hash));
+            };
+
+            var kbd = createTag("kbd", { className: "key" });
+
+            kbd.appendChild(span);
+            kbd.appendChild(button);
+            kbd.appendChild(img);
+            div.appendChild(kbd);
         }
-        div.appendChild(kbd);
-        index2 += 1;
-    }
-    index1 += 1; // 这里需要写 += ，不然会陷入死循环
-}
-
-// 监听按键事件
-// 这里有 BUG！！！onkeypress(已弃用) 和 onkeydown 产生的 bug
-document.onkeydown = function (e) {
-    var key = e["key"];
-    website = hash[key];
-    if(!hash[key]){
-        alert("您还没有对键位" + "「" + e["key"] +  "」"+  "绑定网址哦！")
-    }else{
-        // location.href = website; 在当前页打开
-        window.open(website, "_blank");
+        wrapper.appendChild(div);
     }
 }
 
-
-
+function listenToUser(hash) {
+    document.onkeydown = function (e) {
+        var key = e["key"];
+        website = hash[key];
+        if (!hash[key]) {
+            alert("您还没有对键位" + "「" + e["key"] + "」" + "绑定网址哦！");
+        } else {
+            // location.href = website; 在当前页打开
+            window.open(website, "_blank");
+        }
+    };
+}
